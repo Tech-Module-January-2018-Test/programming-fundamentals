1f709bc304e666d8144afde72cf45aad7ff9b0de
Rusalina <rossavelrs@yahoo.com>
2018-03-13 12:44:04 +0200

    documented the code of solution of problem 2

--- intro-and-basic-syntax/exercise/02. Rectangle Area.cs
+++ intro-and-basic-syntax/exercise/02. Rectangle Area.cs
@@ -6,10 +6,12 @@ namespace Problem_02_Rectangle_Area
     {
         static void Main(string[] args)
         {
+			// Here we parse the variables to double
             double width = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
             double result = width * height;
 
+			// Here we print the result
             Console.WriteLine("{0:0.00}", result);
         }
     }
