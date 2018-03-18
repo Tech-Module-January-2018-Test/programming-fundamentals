566da1a0c934192e46993f98dad54577f136228d
Rusalina <rossavelrs@yahoo.com>
2018-03-18 19:50:43 +0200

    changed a variable name in problem 4 -> sugars to sugarContent

--- intro-and-basic-syntax/exercise/04. Beverage Labels.cs
+++ intro-and-basic-syntax/exercise/04. Beverage Labels.cs
@@ -11,12 +11,14 @@ namespace BeverageLabels
             int volume = int.Parse(Console.ReadLine());
             int energy = int.Parse(Console.ReadLine());
             int sugar = int.Parse(Console.ReadLine());
+			
             // Here we calculate the calories and sugar.
             double kcals = volume * energy / 100.0;
-            double sugars = volume * sugar / 100.0;
+            double sugarContent = volume * sugar / 100.0;
+			
             // Here we print the result.
             Console.WriteLine($"{volume}ml {name}:");
-            Console.WriteLine($"{kcals}kcal, {sugars}g sugars");
+            Console.WriteLine($"{kcals}kcal, {sugarContent}g sugars");
 
 
         }
