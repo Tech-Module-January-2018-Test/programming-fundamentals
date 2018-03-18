96ddcc1bc59eca9781ce996e893ab172f1cdd9aa
mtsoneva <m.tsoneva@yahoo.com>
2018-03-18 14:22:20 +0200

    documented the code of solution of problem 1

--- intro-and-basic-syntax/exercise/01. Debit Card Number.cs
+++ intro-and-basic-syntax/exercise/01. Debit Card Number.cs
@@ -10,12 +10,12 @@ namespace EXC_01.Debit_Card_Number
 	{
 		static void Main(string[] args)
 		{
-
+            // Here we read the numbers from the Console.
 			int firstNumber = int.Parse(Console.ReadLine());
 			int secondNumber = int.Parse(Console.ReadLine());
 			int thirdNumber = int.Parse(Console.ReadLine());
 			int fourthNumber = int.Parse(Console.ReadLine());
-
+            // Here we print the numbers in the required format.
 			Console.WriteLine($"{firstNum:D4} {secondNum:D4} {thirdNum:D4} {fourthNum:D4}");
 		}
 	}
