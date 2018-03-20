4b53801710d70e06c41e64abd4bea68bc2d77f4d
Svetoslav Velev <svetoslav_velev89@hotmail.com>
2018-03-20 21:39:15 +0200

    Documented code of problem 5

--- intro-and-basic-syntax/exercise/05. Character Stats.cs
+++ intro-and-basic-syntax/exercise/05. Character Stats.cs
@@ -12,17 +12,21 @@ namespace EXC_05.Character_Stats
 	{
 		static void Main(string[] args)
 		{
+            // Here we read from the Console.
+
 			string name = Console.ReadLine();
 			int currentHealth = int.Parse(Console.ReadLine());
 			int maxHealth = int.Parse(Console.ReadLine());
 			int currentEnergy = int.Parse(Console.ReadLine());
 			int maxEnergy = int.Parse(Console.ReadLine());
 
+            // Here we calculate health and energy.
 			string healthAsText = new string('|',currentHealth);
 			string healthMissing = new string('.', maxHealth-currentHealth);
 			string energyAsText = new string('|', currentEnergy);
 			string energyMissing = new string('.', maxEnergy - currentEnergy);
 
+            // Here we print the result in required format.
 			Console.WriteLine($"Name: {name}");
 			Console.WriteLine($"Health: |{healthAsText}{healthMissing}|");
 			Console.WriteLine($"Energy: |{energyAsText}{energyMissing}|");
